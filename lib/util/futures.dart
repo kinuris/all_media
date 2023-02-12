f88b5222df96e2725713e7bdf5016458b2dfc88f
chanc <chanchrisdominic@gmail.com>
2023-02-12 11:52:21 +0800

    add builder for check volume size cache

--- lib/util/futures.dart
+++ lib/util/futures.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:v_2_all_media/util/arguments.dart';
 import 'package:v_2_all_media/util/computations.dart';
@@ -38,6 +39,35 @@ Widget waitForFuture(
       }
 
       return builder(context);
+    },
+  );
+}
+
+Widget doesVolumeHaveCachedSizes(
+    {Widget loading = const Placeholder(),
+    required String absoluteVolumeReadPath,
+    required Widget Function(BuildContext context, bool cached) builder}) {
+  return tempDirectoryProvider(
+    loading: loading,
+    builder: (context, tempDir) {
+      final seriesName =
+          absoluteVolumeReadPath.split("/").reversed.skip(1).first;
+      final volumeName = basenameWithoutExtension(
+          absoluteVolumeReadPath.split("/").reversed.first);
+
+      final cachedSizes =
+          File("${tempDir.path}/$seriesName/$volumeName/sizes.json");
+
+      return FutureBuilder(
+        future: cachedSizes.exists(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return loading;
+          }
+
+          return builder(context, snapshot.data!);
+        },
+      );
     },
   );
 }
